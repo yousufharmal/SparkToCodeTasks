317228bf9577af296552e9c8525a03a89d287014
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-01 09:16:48 +0400

    Finished task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Task 1
+
+        string name = "Ahmed";
+        int age = 20;
+        double height = 1.80;
+        bool isStudent =  true;
+        
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Age: " + age);
+        Console.WriteLine("Height: " + height);
+        Console.WriteLine("Student: " + isStudent);
+    }
+}
