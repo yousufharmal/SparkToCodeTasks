424811c3676108303159f66e7ad560c06d333030
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-01 20:52:11 +0400

    Task 11 is done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,51 +2,52 @@
 
 class Program
 {
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
         /*
-         
+
         // Task 1
 
         string name = "Ahmed";
         int age = 20;
         double height = 1.80;
         bool isStudent =  true;
-        
+
         Console.WriteLine("Name: " + name);
         Console.WriteLine("Age: " + age);
         Console.WriteLine("Height: " + height);
         Console.WriteLine("Student: " + isStudent);
-        
-        
+
+
         // Task 2
         Console.Write("Please enter the length of the rectangle: ");
         float length = float.Parse(Console.ReadLine());
         Console.Write("Please enter the width of the rectangle: ");
         float width = float.Parse(Console.ReadLine());
-        
+
         float area = (length * width);
         float perimeter = (length + width) * 2;
-        
+
         Console.WriteLine($"The area of the rectangle is: {area}");
         Console.WriteLine($"The perimeter of the rectangle is: {perimeter}");
-        
-        
+
+
         // Task 3
-        
+
         Console.Write("Please enter a whole number: ");
         int number = int.Parse(Console.ReadLine());
-        
+
         if (number % 2 == 0) {
             Console.WriteLine("The number is even");
         }
         else {
             Console.WriteLine("The number is odd");
         }
-        
-        
-        
+
+
+
         // Task 4
-        
+
         Console.Write("Please enter your age: ");
         int age = int.Parse(Console.ReadLine());
         Console.Write("Do you have a valid National ID ?");
@@ -62,11 +63,11 @@ class Program
         {
             Console.WriteLine("Sorry,you are not eligible to vote!");
         }
-        
-        
-        
+
+
+
         // Task 5
-        
+
         Console.Write("Please enter your grade ('A', 'B', 'C', 'D', or 'F'): ");
         char grade = char.Parse(Console.ReadLine().ToUpper());
 
@@ -90,8 +91,8 @@ class Program
             default:
                 Console.WriteLine("Invalid grade");
                 break;
-                
-             
+
+
         // Task 6
         Console.Write("Enter the temperature in Celsius: ");
         double celsius = double.Parse(Console.ReadLine());
@@ -109,53 +110,53 @@ class Program
         {
             Console.WriteLine("The weather is Hot");
         }
-        
+
         Console.WriteLine($"The weather is {fahrenheit} fahrenheit.");
-        
-        
-        
+
+
+
         // Task 7
-        
+
         Console.Write("Please enter your age: ");
         int age = int.Parse(Console.ReadLine());
 
         if (age > 0 && age <= 12)
         {
             Console.WriteLine("You are a Child, Your movie ticket price is : 2.000OMR");
-            
+
         }
         else if (age >= 13 && age <= 59)
         {
-            Console.WriteLine("You are an Adult, Your movie ticket price is : 5.000OMR"); 
+            Console.WriteLine("You are an Adult, Your movie ticket price is : 5.000OMR");
         }
         else  if (age >= 60)
         {
             Console.WriteLine("You are a Senior, Your movie ticket price is : 3.000OMR");
         }
-        
-        
-        
+
+
+
         // Task 8
-        
+
         Console.Write("Please enter your total bill amount OMR: ");
         double amount = double.Parse(Console.ReadLine());
-        
+
         Console.Write("Are you a loyalty member? yes/no: ");
         string userInput = Console.ReadLine().ToLower();
         double discount = 0;
         if (userInput == "yes" && amount > 20)
         {
              discount = amount * 0.15;
-            
+
         }
-        
-        
+
+
         Console.WriteLine("The original bill in OMR: " + amount);
         Console.WriteLine("The discount amount in OMR: " + discount);
         double total = amount - discount;
         Console.WriteLine("The total bill in OMR: " + total);
-        
-        
+
+
         // Task 9
         Console.WriteLine("Please enter a number between 1 and 7: ");
         int number = int.Parse(Console.ReadLine());
@@ -187,11 +188,11 @@ class Program
                 Console.WriteLine("Invalid day number!");
                 break;
         }
-        
-        */
-        
+
+
+
         // Task 10
-        
+
         Console.Write("please enter the first number: ");
         int firstNumber = int.Parse(Console.ReadLine());
         Console.Write("please enter the second number: ");
@@ -234,6 +235,42 @@ class Program
                 Console.WriteLine("Invalid operator.");
                 break;
         }
+        */
+
+        // Task 11
+
+        Console.Write("please enter your age: ");
+        int age = int.Parse(Console.ReadLine());
+        Console.Write("Please enter your monthly income: ");
+        float monthlyIncome = float.Parse(Console.ReadLine());
+        Console.Write("Do you have an existing loan ? yes/no ");
+        string existingLoan = Console.ReadLine().ToLower();
+
+        bool isLoan = existingLoan == "yes" || existingLoan == "y";
+
+        if (!isLoan && monthlyIncome >= 400 && age >= 21 && age <= 60)
+        {
+            Console.WriteLine("You are eligible for a loan");
+        }
+        else if (isLoan)
+        {
+            Console.WriteLine("You are not eligible for a loan because you have an existing loan!");
+        }
+        else if (monthlyIncome < 400)
+        {
+            Console.WriteLine("You are not eligible for a loan because your monthly income is less than 400 OMR!");
+        }
+        else if (age < 21)
+        {
+            Console.WriteLine("You are not eligible for a loan because you are younger than 21!");
+        }
+        else if (age > 60)
+        {
+            Console.WriteLine("You are not eligible for a loan because you are older than 60!");
+        }
+        
+
+
 
 
     }
