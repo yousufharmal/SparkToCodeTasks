034476c93dbd0521e9d107d3854d470c80301ef3
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-07 02:52:21 +0400

    Task 1 - Countdown Timer is done

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task 1 - Countdown Timer
+        
+        Console.Write("Please enter a number :");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = n; i > 0; i--)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Liftoff!");
     }
 }
