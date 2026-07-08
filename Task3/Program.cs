f3cd0850fedd26cdf4be31183893ca1cab46d5c2
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-08 19:05:52 +0400

    Task 1 - Absolute Difference is done

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,7 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task 1 - Absolute Difference //////////////
+        
+        Console.Write("Please enter your first number: ");
+        double firstNumber = double.Parse(Console.ReadLine());
+        Console.Write("Please enter your second number: ");
+        double secondNumber = double.Parse(Console.ReadLine());
+        
+        double subtraction = firstNumber - secondNumber;
+        double absoulute = Math.Abs(subtraction);
+        
+        Console.WriteLine("Your final positive difference is: " + absoulute);
         
         
     }
