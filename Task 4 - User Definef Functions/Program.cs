b61bb27b818d0666df8c045ce52e6bd6d67747f0
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-10 22:27:17 +0400

    Task 2 - Square Number Function is done

--- Task 4 - User Definef Functions/Program.cs
+++ Task 4 - User Definef Functions/Program.cs
@@ -3,15 +3,31 @@
 class Program
 {
     // Task 1 - Personalized Welcome Function
+    
     public static void PrintWelcome(string name)
     {
         Console.WriteLine($"Welcome {name}!");
     }
     
+    
+    // Task 2 - Square Number Function
+    public static int Square(int n)
+    {
+        return n * n;
+    }
     static void Main(string[] args)
     {
+        // // Task 1 - Personalized Welcome Function
+        /*
         Console.Write("Please enter your name: ");
         string input = Console.ReadLine();
         PrintWelcome(input);
+        */
+        
+        // Task 2 - Square Number Function
+        Console.Write("Enter a number: ");
+        int a = int.Parse(Console.ReadLine());
+        Square(a);
+        Console.WriteLine($"The square of {a} is: {Square(a)}");
     }
 }
