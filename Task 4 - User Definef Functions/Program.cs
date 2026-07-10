8027fa472757bedb780f8b42e05c8a48c32b300f
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-10 22:46:21 +0400

    Task 4 - Fixed Menu Display Function is done

--- Task 4 - User Definef Functions/Program.cs
+++ Task 4 - User Definef Functions/Program.cs
@@ -21,6 +21,17 @@ class Program
     {
         return (celsius * 9 / 5) + 32;
     }
+    
+    // Task 4 - Fixed Menu Display Function
+
+    public static void DisplayMenu()
+    {
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("Please choose an option: ");
+        Console.WriteLine("1: Start");
+        Console.WriteLine("2: Help");
+        Console.WriteLine("3: Exit");
+    }
     static void Main(string[] args)
     {
         // // Task 1 - Personalized Welcome Function
@@ -35,12 +46,18 @@ class Program
         int a = int.Parse(Console.ReadLine());
         Square(a);
         Console.WriteLine($"The square of {a} is: {Square(a)}");
-        */
+        
         
         // Task 3 - Celsius to Fahrenheit Function
         
         Console.Write("Please enter the temperature in Celsius: ");
         double celsius = double.Parse(Console.ReadLine());
         Console.WriteLine($"The temperature in Fehrenheit is {CelsiusToFahrenheit(celsius)}");
+        */
+        
+        // Task 4 - Fixed Menu Display Function
+        DisplayMenu();
+        
+        
     }
 }
