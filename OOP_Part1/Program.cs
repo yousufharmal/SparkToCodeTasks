76dcbc66a7f3547ee15cc50974ef9a4f289919fd
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-20 12:11:22 +0400

    BankAccount class is implemented

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -1,7 +1,42 @@
 ﻿namespace OOP_Part1;
 
 
+public class BankAccount
+{
+    public int AccountNumber { get; set; }
+    public string HolderName { get; set; }
+    public double Balance { get; set; }
+    
+    public void Deposit(double amount) 
+    {
+        Balance += amount;
+        SendEmail();
+    }
 
+    public void Withdraw(double amount)
+    {
+        Balance -= amount;
+        SendEmail();
+    }
+
+    public double CheckBalance()
+    {
+        PrintInformation();
+        return Balance;
+    }
+
+    private void PrintInformation()
+    {
+       
+        Console.WriteLine($"Holder Name: {HolderName}");
+        Console.WriteLine($"Balance: {Balance}");
+    }
+    
+    private void SendEmail()
+    {
+        Console.WriteLine("An email has been sent to your email address");
+    }
+}
 
 public class Program
 {
