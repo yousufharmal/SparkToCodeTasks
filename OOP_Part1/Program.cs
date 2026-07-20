92f9327171a839fe8f77697ee050503b75f19f89
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-20 16:07:27 +0400

    Case 15 is done

--- OOP_Part1/Program.cs
+++ OOP_Part1/Program.cs
@@ -527,6 +527,27 @@ public class Program
             Console.WriteLine("Not enough balance");
         }
     }
+    
+    static void FullBalanceTopUpFlow()
+    {
+        BankAccount account = ChooseAccount();
+
+        if (account.Balance < 50)
+        {
+            double before = account.Balance;
+            double topUp = 100 - account.Balance;
+            account.Deposit(topUp);
+            Console.WriteLine("Balance before top-up: " + before);
+            Console.WriteLine("Balance after top-up: " + account.Balance);
+        }
+        else
+        {
+            Console.WriteLine("No top-up needed - balance is already 50 or above.");
+        }
+    }
+    
+    
+    
 
     
 }
