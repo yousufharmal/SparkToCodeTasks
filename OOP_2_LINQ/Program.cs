82733d72e323a4580d99cac8ad9e3448312770ad
Yousuf Alharmali <yousufsaidalharmali@gmail.com>
2026-07-23 16:42:12 +0400

    Class Guest is implemented

--- OOP_2_LINQ/Program.cs
+++ OOP_2_LINQ/Program.cs
@@ -29,6 +29,45 @@ namespace OOP_2_LINQ
                 $"Price: OMR {PricePerNight:F2} | Status: {status}");
         }
     }
+    
+    class Guest
+    {
+        public string GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string RoomNumber { get; set; }
+        public string CheckInDate { get; set; }
+        public int TotalNights { get; set; }
+
+        
+        public double PricePerNight { get; set; }
+
+        public Guest(
+            string guestId,
+            string guestName,
+            string roomNumber,
+            string checkInDate,
+            int totalNights)
+        {
+            GuestId = guestId;
+            GuestName = guestName;
+            RoomNumber = roomNumber;
+            CheckInDate = checkInDate;
+            TotalNights = totalNights;
+            PricePerNight = 0;
+        }
+
+        public void DisplayGuest()
+        {
+            Console.WriteLine(
+                $"ID: {GuestId} | Name: {GuestName} | Room: {RoomNumber} | " +
+                $"Check-in: {CheckInDate} | Nights: {TotalNights}");
+        }
+
+        public double CalculateTotalCost()
+        {
+            return TotalNights * PricePerNight;
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
